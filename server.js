c1028d25085dd9c4146b11ad2bd13d22b0c77014
OLUWANIFEMI <latunde.nifemi2016@gmail.com>
2022-04-25 14:04:03 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,40 @@
 // Static server file
 
-const http = require('http');
+const http = require("http");
+var fs = require("fs");
 
 const port = 4000;
-const localhost = '127.0.0.2';
+const host = "127.0.0.1";
 
 // the server request
 const server = http.createServer((req, res) => {
-  // route request goes in here 
+  const urlPath = req.url;
+  console.log(urlPath);
+  // route request goes in here
+
+  if (urlPath === "/") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    fs.createReadStream("./index.html").pipe(res);
+    res.end();
+  } else if (urlPath === "/about") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    fs.createReadStream("./about.html").pipe(res);
+    res.end();
+  } else if (urlPath === "/sys") {
+    res.statusCode = 201;
+    res.setHeader("Content-Type", "application/json");
+    res.end("Your OS info has been saved successfully!");
+  } else {
+    res.statusCode = 404;
+    res.setHeader("Content-Type", "text/html");
+    fs.createReadStream(__dirname + "/404.html").pipe(res);
+    res.end();
+  }
 });
 
-// the port 
-server.listen(port, localhost, () => {
-    console.log(`server running on ${localhost}:${port}`);
-})
+// the port
+server.listen(port, host, () => {
+  console.log(`server running on ${host}:${port}`);
+});
